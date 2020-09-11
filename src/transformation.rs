ae5ca085bc5df20c6a1fbddebb1160c248b9fc02
Giovanni Berti <dev.giovanniberti@gmail.com>
2020-09-11 20:19:50 +0200

    feat(transformation): add function names transformation

---
+++ src/transformation.rs
@@ -0,0 +1,99 @@
+use crate::validation::JNIBridgeModule;
+use syn::fold::Fold;
+use syn::{ItemMod, ItemImpl, Signature, Type, ItemStruct};
+use proc_macro2::{TokenStream, Ident};
+use quote::ToTokens;
+
+pub(crate) struct ModTransformer {
+    module: JNIBridgeModule
+}
+
+impl ModTransformer {
+    pub(crate) fn new(module: JNIBridgeModule) -> Self {
+        ModTransformer {
+            module
+        }
+    }
+
+    pub(crate) fn transform_module(&mut self) -> TokenStream {
+        let module_decl = self.module.module_decl.clone();
+        self.fold_item_mod(module_decl).into_token_stream()
+    }
+}
+
+impl Fold for ModTransformer {
+    fn fold_item_impl(&mut self, node: ItemImpl) -> ItemImpl {
+        if let Type::Path(p) =  &*node.self_ty {
+            let struct_name = p.path.segments.last().unwrap().ident.to_string();
+            let struct_package = self.module.package_map[&struct_name].clone();
+            let mut struct_transformer = ImplTransformer { struct_name, package: struct_package };
+
+            struct_transformer.fold_item_impl(node)
+        } else {
+            ItemImpl {
+                attrs: node.attrs.into_iter().map(|a| self.fold_attribute(a)).collect(),
+                defaultness: node.defaultness,
+                unsafety: node.unsafety,
+                impl_token: node.impl_token,
+                generics: self.fold_generics(node.generics),
+                trait_: node.trait_,
+                self_ty: Box::new(self.fold_type(*node.self_ty)),
+                brace_token: node.brace_token,
+                items: node.items.into_iter().map(|i| self.fold_impl_item(i)).collect()
+            }
+        }
+    }
+
+    fn fold_item_mod(&mut self, node: ItemMod) -> ItemMod {
+        ItemMod {
+            attrs: vec![],
+            vis: self.fold_visibility(node.vis),
+            mod_token: node.mod_token,
+            ident: self.fold_ident(node.ident),
+            content: node.content.map(|(brace, items)| (brace, items.into_iter().map(|i| self.fold_item(i)).collect())),
+            semi: node.semi
+        }
+    }
+
+    fn fold_item_struct(&mut self, node: ItemStruct) -> ItemStruct {
+        ItemStruct {
+            attrs: vec![],
+            vis: node.vis,
+            struct_token: node.struct_token,
+            ident: node.ident,
+            generics: self.fold_generics(node.generics),
+            fields: self.fold_fields(node.fields),
+            semi_token: node.semi_token
+        }
+    }
+}
+
+struct ImplTransformer {
+    pub(crate) struct_name: String,
+    pub(crate) package: String
+}
+
+impl Fold for ImplTransformer {
+    fn fold_signature(&mut self, node: Signature) -> Signature {
+        let jni_method_name = {
+            let mut res = self.package.clone().replace('.', "_");
+            res.push('_');
+            res.push_str(&self.struct_name);
+            res
+        };
+
+        Signature {
+            constness: node.constness,
+            asyncness: node.asyncness,
+            unsafety: node.unsafety,
+            abi: node.abi,
+            fn_token: node.fn_token,
+            ident: Ident::new(&jni_method_name, node.ident.span()),
+            generics: node.generics,
+            paren_token: node.paren_token,
+            inputs: node.inputs,
+            variadic: node.variadic,
+            output: node.output
+        }
+    }
+}
---
+++ src/validation.rs
@@ -0,0 +1,266 @@
+use core::option::Option::{None, Some};
+use core::result::Result::{Err, Ok};
+use std::collections::BTreeMap;
+
+use proc_macro_error::{emit_error, emit_warning};
+use quote::ToTokens;
+use syn::{Attribute, Error, Ident, Item, ItemMod, ItemStruct, Result, Type, ItemImpl};
+use syn::parse::{Parse, ParseBuffer, ParseStream};
+use syn::punctuated::Punctuated;
+use syn::spanned::Spanned;
+use syn::visit::Visit;
+use syn::Token;
+
+struct AttribItemChecker {
+    valid: bool
+}
+
+impl AttribItemChecker {
+    fn new() -> Self {
+        AttribItemChecker {
+            valid: true
+        }
+    }
+}
+
+impl<'ast> Visit<'ast> for AttribItemChecker {
+    fn visit_item(&mut self, node: &'ast Item) {
+        let has_package_attribute = |a: &Attribute| a.path.segments.first().unwrap().ident.to_string() == "package";
+        match node {
+            Item::Struct(_) => {}
+            Item::Const(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Enum(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type"; help = i.enum_token.span() => "replace `enum` with `struct`");
+                self.valid = false;
+            }
+            Item::ExternCrate(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Fn(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::ForeignMod(i) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Impl(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Macro(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Macro2(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Mod(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Static(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Trait(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::TraitAlias(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Type(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Union(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Use(i) if i.attrs.iter().any(has_package_attribute) => {
+                emit_error!(i.span(), "`package` attribute used on non-struct type");
+                self.valid = false;
+            }
+            Item::Verbatim(_) => {}
+            _ => {}
+        }
+    }
+}
+
+#[derive(Default)]
+struct ImplAccumulator<'ast> {
+    impls: Vec<&'ast ItemImpl>
+}
+
+impl<'ast> Visit<'ast> for ImplAccumulator<'ast> {
+    fn visit_item_impl(&mut self, node: &'ast ItemImpl) {
+        self.impls.push(node);
+    }
+}
+
+enum StructDeclarationKind {
+    // structs with `package` attrib and impl
+    Bridged,
+    // structs with `package` attrib but no impl
+    UnImpl,
+    // structs without `package` attrib but with impl
+    UnAttrib,
+    // structs without `package` attrib and no impl
+    Bare,
+}
+
+struct StructDeclVisitor<'ast> {
+    module_structs: Vec<(&'ast ItemStruct, StructDeclarationKind)>,
+    // all module impls
+    module_impls: Vec<&'ast ItemImpl>,
+}
+
+impl<'ast> StructDeclVisitor<'ast> {
+    fn new(module_impls: Vec<&'ast ItemImpl>) -> Self {
+        StructDeclVisitor {
+            module_structs: Vec::new(),
+            module_impls,
+        }
+    }
+}
+
+impl<'ast> Visit<'ast> for StructDeclVisitor<'ast> {
+    fn visit_item_struct(&mut self, node: &'ast ItemStruct) {
+        let struct_name = node.ident.to_string();
+        let has_package_attrib = node.attrs.iter().any(|a| a.path.segments.first().unwrap().ident.to_string() == "package");
+        let has_impl = self.module_impls.iter().filter_map(|i| {
+            match &*i.self_ty {
+                Type::Path(p) => Some(p.path.segments.last().unwrap().ident.to_string()),
+                _ => None
+            }
+        }).any(|s| s == struct_name);
+
+        let declaration_kind = match (has_package_attrib, has_impl) {
+            (true, true) => StructDeclarationKind::Bridged,
+            (true, false) => StructDeclarationKind::UnImpl,
+            (false, true) => StructDeclarationKind::UnAttrib,
+            (false, false) => StructDeclarationKind::Bare,
+        };
+
+        self.module_structs.push((node, declaration_kind))
+    }
+}
+
+pub(crate) struct JNIBridgeModule {
+    pub(crate) module_decl: ItemMod,
+    pub(crate) package_map: BTreeMap<String, String>,
+    pub(crate) bridged_impls: Vec<(ItemStruct, ItemImpl)>,
+}
+
+impl Parse for JNIBridgeModule {
+    fn parse(input: &ParseBuffer) -> Result<Self> {
+        let mut valid_input;
+        let module_decl: ItemMod = input.parse().map_err(|e| Error::new(e.span(), "`bridge` attribute is supported on mod items only"))?;
+
+        let mut attribute_checker = AttribItemChecker::new();
+        attribute_checker.visit_item_mod(&module_decl);
+        valid_input = attribute_checker.valid;
+
+        let mut impl_visitor = ImplAccumulator::default();
+        impl_visitor.visit_item_mod(&module_decl);
+
+        let mut mod_visitor = StructDeclVisitor::new(impl_visitor.impls);
+        mod_visitor.visit_item_mod(&module_decl);
+
+        let bridged_structs: Vec<_> = mod_visitor.module_structs.into_iter()
+            .filter_map(|(struct_item, decl_kind)| {
+                match decl_kind {
+                    StructDeclarationKind::Bridged => Some(struct_item),
+                    StructDeclarationKind::UnImpl => {
+                        emit_warning!(struct_item, "ignoring struct without declared methods"; help = "add methods using an `impl` block");
+                        None
+                    }
+                    StructDeclarationKind::UnAttrib => {
+                        emit_error!(struct_item, "struct without required `package` attribute");
+                        valid_input = false;
+                        None
+                    }
+                    StructDeclarationKind::Bare => {
+                        emit_warning!(struct_item, "ignoring struct with no `package` attribute and no implementation";
+                            help = struct_item.span() => "add a #[package(...)] attribute";
+                            note = "structs with declared methods require package attribute for correct translation");
+                        None
+                    }
+                }
+            })
+            .collect();
+
+        let structs_idents: Vec<_> = bridged_structs.iter().map(|s| &s.ident).collect();
+        let bridged_impls: Vec<_> = mod_visitor.module_impls.iter()
+            .filter_map(|item_impl| {
+                match &*item_impl.self_ty {
+                    Type::Path(p) => {
+                        if let Some(pos) = structs_idents.iter().position(|id| *id == &p.path.segments.last().unwrap().ident) {
+                            Some((bridged_structs[pos], *item_impl))
+                        } else {
+                            None
+                        }
+                    }
+                    _ => None
+                }
+            })
+            .map(|(s, i)| (s.clone(), i.clone()))
+            .collect();
+
+        mod_visitor.module_impls.into_iter()
+            .filter(|i| {
+                if let Type::Path(p) = &*i.self_ty {
+                    let impl_struct_name = p.path.segments.last().unwrap().ident.to_string();
+                    !bridged_impls.iter()
+                        .map(|(_, i)| i)
+                        .filter_map(|i| {
+                            match &*i.self_ty {
+                                Type::Path(p) => Some(p.path.segments.last().unwrap().ident.to_string()),
+                                _ => None
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .contains(&impl_struct_name)
+                } else {
+                    false
+                }
+        }).for_each(|lone_impl| {
+            emit_error!(lone_impl, "impl declared without corresponding struct");
+            valid_input = false;
+        });
+
+        let package_map: BTreeMap<String, String> = bridged_structs.iter()
+            .map(|s| {
+                let name = s.ident.to_string();
+                let package = s.attrs.iter()
+                    .filter(|a| a.path.segments.last().unwrap().ident.to_string() == "package")
+                    .map(|a| {
+                        a.parse_args_with(|t: ParseStream| { Punctuated::<Ident, Token![.]>::parse_separated_nonempty(t) })
+                            .unwrap()
+                            .to_token_stream()
+                            .to_string().replace(' ', "")
+                    })
+                    .next().unwrap();
+                (name, package)
+            })
+            .collect();
+
+        if !valid_input {
+            Err(Error::new(module_decl.span(), "`bridge` macro expansion failed due to previous errors"))
+        } else {
+            Ok(JNIBridgeModule {
+                module_decl,
+                package_map,
+                bridged_impls,
+            })
+        }
+    }
+}
+
