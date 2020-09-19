f0d0fad876bd7842fcc9854858a3fad1de557863
Giovanni Berti <dev.giovanniberti@gmail.com>
2020-09-19 13:37:32 +0200

    feat(convert::safe): add impls for `Try*` conversions to feature-match `unchecked` variants

--- src/convert/safe.rs
+++ src/convert/safe.rs
@@ -1,5 +1,7 @@
-use jni::errors::Result as Result;
+use jni::errors::{Result as Result};
 use jni::JNIEnv;
+use jni::objects::{JList, JObject, JString, JValue};
+use jni::sys::{jboolean, jbooleanArray, jchar, jobject, jstring};
 
 use crate::convert::JavaValue;
 use crate::convert::unchecked::{FromJavaValue, IntoJavaValue};
@@ -33,3 +35,109 @@ impl<'env, T> TryFromJavaValue<'env> for T where T: JavaValue<'env> {
         Ok(FromJavaValue::from(s, env))
     }
 }
+
+impl<'env> TryIntoJavaValue<'env> for String {
+    type Target = jstring;
+
+    fn try_into(self, env: &JNIEnv<'env>) -> Result<Self::Target> {
+        env.new_string(self).map(|s| s.into_inner())
+    }
+}
+
+impl<'env> TryFromJavaValue<'env> for String {
+    type Source = JString<'env>;
+
+    fn try_from(s: Self::Source, env: &JNIEnv<'env>) -> Result<Self> {
+        env.get_string(s).map(Into::into)
+    }
+}
+
+impl<'env> TryIntoJavaValue<'env> for bool {
+    type Target = jboolean;
+
+    fn try_into(self, _env: &JNIEnv<'env>) -> Result<Self::Target> {
+        Ok(IntoJavaValue::into(self, _env))
+    }
+}
+
+impl<'env> TryFromJavaValue<'env> for bool {
+    type Source = jboolean;
+
+    fn try_from(s: Self::Source, _env: &JNIEnv<'env>) -> Result<Self> {
+        Ok(FromJavaValue::from(s, _env))
+    }
+}
+
+impl<'env> TryIntoJavaValue<'env> for char {
+    type Target = jchar;
+
+    fn try_into(self, _env: &JNIEnv<'env>) -> Result<Self::Target> {
+        Ok(IntoJavaValue::into(self, _env))
+    }
+}
+
+impl<'env> TryFromJavaValue<'env> for char {
+    type Source = jchar;
+
+    fn try_from(s: Self::Source, _env: &JNIEnv<'env>) -> Result<Self> {
+        // TODO: Check validity of implementation unsafe block
+        Ok(FromJavaValue::from(s, _env))
+    }
+}
+
+impl<'env> TryIntoJavaValue<'env> for Box<[bool]> {
+    type Target = jbooleanArray;
+
+    fn try_into(self, env: &JNIEnv<'env>) -> Result<Self::Target> {
+        let len = self.len();
+        let buf: Vec<_> = self.iter().map(|&b| Into::into(b)).collect();
+        let raw = env.new_boolean_array(len as i32)?;
+        env.set_boolean_array_region(raw, 0, &buf)?;
+        Ok(raw)
+    }
+}
+
+impl<'env> TryFromJavaValue<'env> for Box<[bool]> {
+    type Source = jbooleanArray;
+
+    fn try_from(s: Self::Source, env: &JNIEnv<'env>) -> Result<Self>{
+        let len = env.get_array_length(s)?;
+        let mut buf = Vec::with_capacity(len as usize).into_boxed_slice();
+        env.get_boolean_array_region(s, 0, &mut *buf)?;
+
+        buf.iter().map(|&b| TryFromJavaValue::try_from(b, &env)).collect()
+    }
+}
+
+impl<'env, T> TryIntoJavaValue<'env> for Vec<T> where T: TryIntoJavaValue<'env> {
+    type Target = jobject;
+
+    fn try_into(self, env: &JNIEnv<'env>) -> Result<Self::Target> {
+        let obj = env.new_object("java/util/ArrayList", "(I)V", &[JValue::Int(self.len() as i32)])?;
+        let list = JList::from_env(&env, obj)?;
+
+        let _: Result<Vec<_>> = self.into_iter()
+            .map::<Result<_>, _>(|el| Ok(JavaValue::autobox(TryIntoJavaValue::try_into(el, &env)?, &env)))
+            .map(|el| {
+                Ok(list.add(el?))
+            })
+            .collect();
+
+        Ok(list.into_inner())
+    }
+}
+
+impl<'env, T, U> TryFromJavaValue<'env> for Vec<T> where T: TryFromJavaValue<'env, Source=U>, U: JavaValue<'env> {
+    type Source = JObject<'env>;
+
+    fn try_from(s: Self::Source, env: &JNIEnv<'env>) -> Result<Self>{
+        let list = JList::from_env(env, s)?;
+
+        list.iter()?
+            .map(|el| {
+                Ok(T::try_from(U::unbox(el, env), env)?)
+            })
+            .collect()
+    }
+}
+
