e5fbf63c014923eec239c03c01f48b3e7c244b8f
Giovanni Berti <dev.giovanniberti@gmail.com>
2020-09-19 14:18:58 +0200

    refactor(example): update example

--- robusta-example/src/lib.rs
+++ robusta-example/src/lib.rs
@@ -6,10 +6,10 @@ mod jni {
     struct HelloWorld;
 
     impl HelloWorld {
-        fn special(mut input1: i32, input2: i32) -> i32 {
-            /*input1.push(input2);
-            input1.iter().map(ToString::to_string).collect()*/
-            input1 + input2
+        #[call_type(safe)]
+        fn special(mut input1: Vec<i32>, input2: i32) -> Vec<String> {
+            input1.push(input2);
+            input1.iter().map(ToString::to_string).collect()
         }
     }
 }
