8cb058556997a9a5c5b8d6d0bede16adc3644bda
Andrew Houts <ahouts4@gmail.com>
2022-02-08 23:27:10 -0600

    remove star import

--- tests/driver/src/test/java/UserTest.java
+++ tests/driver/src/test/java/UserTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserTest {
     private User u;
