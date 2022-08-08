85605836e725b8bcaadf5f94806db33ed61c618c
Elise Chouleur <elise.chouleur@doctolib.com>
2022-08-08 16:27:21 +0200

    remove warnings

--- robusta-android-example/src/lib.rs
+++ robusta-android-example/src/lib.rs
@@ -5,11 +5,10 @@ mod jni {
     use android_logger::Config;
     use jni::objects::JObject;
     use log::info;
-    use robusta_jni::convert::{Signature, IntoJavaValue, FromJavaValue, TryIntoJavaValue, TryFromJavaValue};
+    use robusta_jni::convert::{Signature, IntoJavaValue, TryIntoJavaValue, TryFromJavaValue};
     use robusta_jni::jni::JNIEnv;
     use robusta_jni::jni::objects::AutoLocal;
     use robusta_jni::jni::errors::Result as JniResult;
-    use robusta_jni::jni::errors::Error as JniError;
 
     #[derive(Signature, TryIntoJavaValue, IntoJavaValue, TryFromJavaValue)]
     #[package(com.example.robustaandroidexample)]
