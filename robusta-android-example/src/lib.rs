00199ee4816fd8c7546edc6e482086e2563d2aa8
Elise Chouleur <elise.chouleur@doctolib.com>
2023-10-31 17:49:48 +0100

    with_min_level doesn't exist anymore

--- robusta-android-example/src/lib.rs
+++ robusta-android-example/src/lib.rs
@@ -28,11 +28,7 @@ mod jni {
 
     impl<'env: 'borrow, 'borrow> RobustaAndroidExample<'env, 'borrow> {
         pub extern "jni" fn runRustExample(self, env: &JNIEnv, context: JObject<'env>) {
-            android_logger::init_once(
-                Config::default()
-                    .with_min_level(log::Level::Debug)
-                    .with_tag("RUST_ROBUSTA_ANDROID_EXAMPLE"),
-            );
+            android_logger::init_once(Config::default().with_tag("RUST_ROBUSTA_ANDROID_EXAMPLE"));
 
             info!("TEST START");
             let java_class = env
